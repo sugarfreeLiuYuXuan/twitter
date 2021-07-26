64ac2b6e4d2585f109c33690a2bed7ac98746dd6
sugarfreeLiuYuXuan <562048502@qq.com>
2021-07-26 12:57:27 +0800

    add login and sign up

---
+++ twittor/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+from twittor import db, login_manager
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    tweets = db.relationship('Tweet', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return 'id={}, username={}, email={}, password_hash={}'.format(
+            self.id, self.username, self.email, self.password_hash
+        )
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "id={}, body={}, create_time={}, user_id={}".format(
+            self.id, self.body, self.create_time, self.user_id
+        )
---
+++ twittor/route.py
@@ -0,0 +1,57 @@
+from flask import render_template, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+from twittor.forms import LoginForm, RegisterForm
+from twittor.models import User, Tweet, load_user
+from twittor import db
+
+@login_required
+def index():
+    posts = [
+        {
+            'author': {'username': 'root'},
+            'body': "hi I'm root!"
+        },
+        {
+            'author': {'username': 'test'},
+            'body': "hi I'm test!"
+        },
+        {
+            'author': {'username': 'test1'},
+            'body': "hi I'm test1!"            
+        }      
+    ]
+    return render_template('index.html', posts=posts)
+
+
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        u = User.query.filter_by(username=form.username.data).first()
+        if u is None or not u.check_password(form.password.data):
+            print('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(u, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(next_page)
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
+
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registration', form=form)
